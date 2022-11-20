92c0243d79b211629a56657fc120b9ea958ba3b0
Alisson Aguiar <alisson-aguiars2k@outlook.com>
2022-11-20 10:36:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Gallery OOP !  🧑 🧔 👧
+<br>
+
+## Gallery, um aplicação simples, quem tem como objetivo mostrar fotos em forma de galeria com o conceito OOP. 💻 📚
+<br>
+
+## Tecnologias que utilizei para o projeto ;  
+<div style="display: inline_block"><br>
+    <img  align="center" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" heigth="30" width="40"alt="HTML5">
+    <img  align="center" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" heigth="30" width="40"alt="CSS3">
+    <img  align="center" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" heigth="30" width="40"alt="JavaScript">
+</div>
+
+<br>
+
+## Projeto 🥰
+
+![](./images/galery-oop.jpg)
+
+### [Gallery OOP](https://alisson-aguiars2k.github.io/gallery-oop/) 🔗
---
+++ app.js
@@ -0,0 +1,104 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
+}
+
+function Gallery(element) {
+  // console.log(element);
+  this.container = element;
+  // console.log(this.container);
+  this.list = [...element.querySelectorAll(".img")];
+  // console.log(this.list);
+  // targe.(alvo.)
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.imageName = getElement(".image-name");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+  // self ref
+  // let self = this;
+  // bind functions
+  // this.openModal = this.openModal.bind(this)
+  this.closeModal = this.closeModal.bind(this);
+  this.prevImage = this.prevImage.bind(this);
+  this.nextImage = this.nextImage.bind(this);
+  this.chooseImage = this.chooseImage.bind(this);
+  // conatiner event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.classList.contains("img")) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
+
+Gallery.prototype.openModal = function (selectedImage, list) {
+  // console.log(selectedImage, list);
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list
+    .map(function (image) {
+      return `<img src="${
+        image.src
+      }" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}"/> `;
+    })
+    .join("");
+  this.modal.classList.add("open");
+  this.closeBtn.addEventListener("click", this.closeModal);
+  this.prevBtn.addEventListener("click", this.prevImage);
+  this.nextBtn.addEventListener("click", this.nextImage);
+  this.modalImages.addEventListener("click", this.chooseImage);
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
+};
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove("open");
+  this.closeBtn.removeEventListener("click", this.closeModal);
+  this.prevBtn.removeEventListener("click", this.prevImage);
+  this.nextBtn.removeEventListener("click", this.nextImage);
+  this.modalImages.removeEventListener("click", this.chooseImage);
+};
+
+Gallery.prototype.prevImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  const prev =
+    selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove("selected");
+  prev.classList.add("selected");
+  this.setMainImage(prev);
+};
+
+Gallery.prototype.nextImage = function () {
+  const selected = this.modalImages.querySelector(".selected");
+  const next =
+    selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove("selected");
+  next.classList.add("selected");
+  this.setMainImage(next);
+};
+
+Gallery.prototype.chooseImage = function (e) {
+  if (e.target.classList.contains('modal-img')) {
+    // console.log(e.target);
+    const selected = this.modalImages.querySelector(".selected");
+    selected.classList.remove("selected");
+
+    this.setMainImage(e.target);
+    e.target.classList.add("selected");
+  }
+};
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
